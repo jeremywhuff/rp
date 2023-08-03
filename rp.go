f989cd9e4f52261a10217358732eb2aa6ad27bc2
Jeremy Huff <jeremy@purplego.co>
2023-08-03 11:45:22 -0700

    Added ToTime stage.

--- rp.go
+++ rp.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"errors"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -333,6 +334,45 @@ func ToObjectID() *Stage {
 
 		F: func(in any, c *gin.Context, lgr Logger) (any, error) {
 			return primitive.ObjectIDFromHex(in.(string))
+		},
+
+		E: func(err error) *StageError {
+			return &StageError{
+				Code: BR,
+				Obj:  H{"error": "Invalid: " + err.Error()},
+			}
+		},
+	}
+}
+
+// ToTime - Converts in to time.Time. in must be a string. It will be parsed per the given timezone and layout.
+// If ctxTimezoneName is "" or its value has not been set in the context, UTC will be used.
+func ToTime(ctxTimezoneName string, layout string) *Stage {
+	return &Stage{
+
+		P: func() string {
+			return "  => .(time.Time) =>"
+		},
+
+		F: func(in any, c *gin.Context, lgr Logger) (any, error) {
+
+			timeString, ok := in.(string)
+			if !ok {
+				return nil, errors.New("not a string")
+			}
+
+			timezoneName := ""
+			if tz, ok := c.Get(ctxTimezoneName); ok {
+				timezoneName = tz.(string)
+			}
+
+			locationTimezone, err := time.LoadLocation(timezoneName)
+			if err != nil {
+				// Default to UTC
+				locationTimezone, _ = time.LoadLocation("UTC")
+			}
+
+			return time.ParseInLocation(layout, timeString, locationTimezone)
 		},
 
 		E: func(err error) *StageError {
